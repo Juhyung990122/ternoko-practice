9e9ec58ec55b0b8f84def5c3b286757b0add3bf7
Juhyung990122 <harukawa99@naver.com>
2022-07-03 15:27:10 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/preternoko/PreternokoApplication.java
@@ -0,0 +1,13 @@
+package com.example.preternoko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreternokoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreternokoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/preternoko/PreternokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preternoko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreternokoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
